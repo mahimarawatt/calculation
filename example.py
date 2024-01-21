e24124f9f16a40c16919f4da2523fcbcc2a21f29
mahimarawatt <mahimarawat22@gmail.com>
2024-01-21 23:04:53 +0530

    updated file

--- example.py
+++ example.py
@@ -1,8 +1,12 @@
-
-a = 10
-b = 30
+a = int(input("enter the first number"))
+b = int(input("enter the second number"))
 print("This is a example file")
 sum = a+b
-sub = b-a
+sub = a-b
+mul = a*b
+div = a/b
 print("sum is : ", sum)
 print("substraction is :", sub)
+print("Multiplication is :", mul)
+print("division is :", div )
+print("updated ")
